bc66d3d6b59eb3c19a7f2f108c7593ade1e4228f
Boris Huang <scps880510@gmail.com>
2021-11-23 16:13:21 +0800

    新增Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    ©2021 Go Go Bus. All Rights Reserved. Design By Tracy. Code By Boris.
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+.footer {
+  text-align: center;
+  font-weight: bold;
+  font-size: 18px;
+  color: #d9d9d9;
+  margin-top: 25px;
+}
+</style>
